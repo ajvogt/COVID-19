718f74ebdb94119541444236b4ebe8e9ec6d7a51
ajvogt <vogtadamj@gmail.com>
2020-07-15 22:01:52 -0500

    finishing methods to pull & refine time series data

--- ajvogt-analysis/mo_analysis_script.py
+++ ajvogt-analysis/mo_analysis_script.py
@@ -20,35 +20,56 @@ DEFAULTS = {
     }
 }
 
-class RegionalAnalysis(Object):
-    def __init__(self, data_path, stat_area_path):
-        self.data_path = data_path
-        self.stat_area_path = stat_area_path
-        self.stat_area_map = pd.DataFrame()
-        self.stat_area_data = pd.DataFrame()
-        self.state_data = pd.DataFrame()
-        self.county_data = pd.DataFrame()
+class RegionalAnalysis(object):
+    def __init__(self, time_series_path, 
+                 daily_reports_path, stat_area_path):
+        self.time_series_path_ = time_series_path
+        self.daily_reports_path_ = daily_reports_path
+        self.stat_area_path_ = stat_area_path
+        self.stat_area_map_ = pd.DataFrame()
+        self.daily_reports_ = pd.DataFrame()
+        self.time_series_deaths_ = pd.DataFrame()
+        self.time_series_cases_ = pd.DataFrame()
+        self.time_series_files_ = {
+            'deaths': 'csse_covid_19_time_series/time_series_covid19_deaths_US.csv',
+            'cases': 'csse_covid_19_time_series/time_series_covid19_confirmed_US.csv'
+        }
 
     def _pull_stat_area_map(self):
-        return pd.read_csv(self.stat_area_path+'statistical_areas.csv')
+        self.stat_area_map_ = pd.read_csv(self.stat_area_path_+'statistical_areas.csv')
 
-    def _pull_state_data(self):
+    def _join_stat_areas(self, df):
+        if self.stat_area_map_.empty:
+            self._pull_stat_area_map()
 
-        df = pd.read_csv(self.data_path+'csse_covid_19_time_series.csv')
-        df = df[df.Province_State == 'Missouri']
+        df = df.join(
+            self.stat_area_map_.set_index(['Province_State', 'Admin2']),
+            how='left', on=['Province_State', 'Admin2']
+        )
 
         return df
 
-    def _pull_county_data(self):
+    def _pull_time_series(self):
+        for k in self.time_series_files_.keys():
+
+            df = pd.read_csv(self.time_series_path_+self.time_series_files_[k])
+            df = self._join_stat_areas(df)
+            df = df[
+                (df.Province_State == 'Missouri')|
+                (df.MSA.notnull())
+            ]
+            df.loc[df.Admin2.isin(['Out of MO', 'Unassigned']), 'MSA'] = 'Unassigned/Out of MO'
+            df.loc[
+                (df.Province_State == 'Missouri')&
+                (df.MSA.isnull()),
+                'MSA'
+            ] = 'Missouri non-MSA'
+            attr = [x for x in dir(self) if k in x][0]
+            self.__dict__[attr] = df
+
+    def _pull_daily_report_data(self):
 
         pass
-    
-    def pull_all_data(self):
-
-        self.stat_area_map = self._pull_stat_area_map()
-        self.state_data = self._pull_state_data()
-        self.county_data = self._pull_county_data()
-        self.stat_area_data = self._compile_stat_area_data()
 
 
 def plot_daily_info(path, msa, data='deaths'):
@@ -163,6 +184,14 @@ def write_markdown(filename, msa):
 
 
 if __name__ == "__main__":
+
+    ra = RegionalAnalysis(
+        time_series_path='../csse_covid_19_data/',
+        daily_reports_path=None,
+        stat_area_path='data/'
+    )
+    ra._pull_time_series()
+    import pdb; pdb.set_trace()
     print('===== Running MO COVID-19 Analysis Script =====')
     path = '../csse_covid_19_data/csse_covid_19_time_series/'
 
